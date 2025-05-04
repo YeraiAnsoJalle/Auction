0ced5d9a65689c856cd93c1a2543142b465b10cb
YeraiAnsoJalle <yansojalle1@educacion.navarra.es>
2025-05-04 12:27:38 +0200

    He creado las consultas de creacion, eliminacion y modificacion de usuario

--- src/main/java/edu/masanz/da/au/controller/BDController.java
+++ src/main/java/edu/masanz/da/au/controller/BDController.java
@@ -69,4 +69,56 @@ public class BDController {
         }
     }
 
+    public void crearUsuario(String nombre, String password, boolean admin) {
+        try (Connection conn = conectar()) {
+            String sql = "INSERT INTO usuarios (nombre, password, admin) VALUES (?, ?, ?)";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, nombre);
+            pst.setString(2, password);
+            pst.setBoolean(3, admin);
+            pst.executeUpdate();
+            System.out.println("Usuario creado.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void modificarPasswordUsuario(String nombre, String nuevaPassword) {
+        try (Connection conn = conectar()) {
+            String sql = "UPDATE usuarios SET password = ? WHERE nombre = ?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, nuevaPassword);
+            pst.setString(2, nombre);
+            pst.executeUpdate();
+            System.out.println("Contraseña modificada.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void modificarRolUsuario(String nombre, String nuevoRol) {
+        try (Connection conn = conectar()) {
+            String sql = "UPDATE usuarios SET admin = ? WHERE nombre = ?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setBoolean(1, nuevoRol.equalsIgnoreCase("admin"));
+            pst.setString(2, nombre);
+            pst.executeUpdate();
+            System.out.println("Rol modificado.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminarUsuario(String nombre) {
+        try (Connection conn = conectar()) {
+            String sql = "DELETE FROM usuarios WHERE nombre = ?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, nombre);
+            pst.executeUpdate();
+            System.out.println("Usuario eliminado.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
